70e6d656b6b0556f59d27f7b6038d3907b81738c
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-09-05 09:05:21 +0530

    starting at 5 sep

--- backend/prisma/seedTemplates.js
+++ backend/prisma/seedTemplates.js
@@ -4,24 +4,24 @@ import { createWebpageService } from "../src/modules/content/content.service.js"
 
 import Catering from "./templates/catering.json" assert {type: "json"}
 import CateringLocationOne from "./templates/cateringLocationone.json" assert {type: "json"}
-import CateringLocationTwo from "./templates/cateringLocationtwo.json" assert {type: "json"}
-import CateringLocationThree from "./templates/cateringLocationthree.json" assert {type: "json"}
+// import CateringLocationTwo from "./templates/cateringLocationtwo.json" assert {type: "json"}
+// import CateringLocationThree from "./templates/cateringLocationthree.json" assert {type: "json"}
 import ContactUs from "./templates/contactUs.json" assert {type: "json"}
 import LandingPageLocationOne from "./templates/landingPageLocationone.json" assert {type: "json"}
-import LandingPageLocationTwo from "./templates/landingPageLocationtwo.json" assert {type: "json"}
-import LandingPageLocationThree from "./templates/landingPageLocationthree.json" assert {type: "json"}
+// import LandingPageLocationTwo from "./templates/landingPageLocationtwo.json" assert {type: "json"}
+// import LandingPageLocationThree from "./templates/landingPageLocationthree.json" assert {type: "json"}
 import orderOnlineLocationOne from "./templates/orderOnlineLocationone.json" assert {type: "json"}
-import orderOnlineLocationTwo from "./templates/orderOnlineLocationtwo.json" assert {type: "json"}
-import orderOnlineLocationThree from "./templates/orderOnlineLocationthree.json" assert {type: "json"}
+// import orderOnlineLocationTwo from "./templates/orderOnlineLocationtwo.json" assert {type: "json"}
+// import orderOnlineLocationThree from "./templates/orderOnlineLocationthree.json" assert {type: "json"}
 import ourCulture from "./templates/ourCulture.json" assert {type: "json"}
 import ourGroup from "./templates/ourGroup.json" assert {type: "json"}
 import privateDinning from "./templates/privateDinning.json" assert {type: "json"}
 import privateDinningLocationOne from "./templates/privateDinningLocationone.json" assert {type: "json"}
-import privateDinningLocationTwo from "./templates/privateDinningLocationtwo.json" assert {type: "json"}
-import privateDinningLocationThree from "./templates/privateDinningLocationthree.json" assert {type: "json"}
+// import privateDinningLocationTwo from "./templates/privateDinningLocationtwo.json" assert {type: "json"}
+// import privateDinningLocationThree from "./templates/privateDinningLocationthree.json" assert {type: "json"}
 import reserveTableLocationOne from "./templates/reserveTableLocationone.json" assert {type: "json"}
-import reserveTableLocationTwo from "./templates/reserveTableLocationtwo.json" assert {type: "json"}
-import reserveTableLocationThree from "./templates/reserveTableLocationthree.json" assert {type: "json"}
+// import reserveTableLocationTwo from "./templates/reserveTableLocationtwo.json" assert {type: "json"}
+// import reserveTableLocationThree from "./templates/reserveTableLocationthree.json" assert {type: "json"}
 // import Catering from "./templates/catering.json" assert {type: "json"}
 // import Catering from "./templates/catering.json" assert {type: "json"}
 
@@ -30,24 +30,24 @@ import reserveTableLocationThree from "./templates/reserveTableLocationthree.jso
 const webpages = [
   // Catering,
   CateringLocationOne,
-  CateringLocationTwo,
-  CateringLocationThree,
+  // CateringLocationTwo,
+  // CateringLocationThree,
   ContactUs,
   LandingPageLocationOne,
-  LandingPageLocationTwo,
-  LandingPageLocationThree,
+  // LandingPageLocationTwo,
+  // LandingPageLocationThree,
   orderOnlineLocationOne,
-  orderOnlineLocationTwo,
-  orderOnlineLocationThree,
+  // orderOnlineLocationTwo,
+  // orderOnlineLocationThree,
   ourCulture,
   ourGroup,
   privateDinning,
   privateDinningLocationOne,
-  privateDinningLocationTwo,
-  privateDinningLocationThree,
+  // privateDinningLocationTwo,
+  // privateDinningLocationThree,
   reserveTableLocationOne,
-  reserveTableLocationTwo,
-  reserveTableLocationThree
+  // reserveTableLocationTwo,
+  // reserveTableLocationThree
 ]
 
 // Function to recursively replace all "id" fields with new UUIDs
@@ -82,4 +82,4 @@ async function processWebpages() {
   }
 }
 
-processWebpages();
+processWebpages().then(() => console.log("Pages created successfully"));
